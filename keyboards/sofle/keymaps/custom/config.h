4f3d7f5cdafcbcc782b9fb18b9ba703fed688de9
Bruno Romero <bruunobromero@gmail.com>
2026-05-12 10:36:31 -0300

    :tada:

--- keyboards/sofle/keymaps/custom/config.h
+++ keyboards/sofle/keymaps/custom/config.h
@@ -6,3 +6,7 @@
 #define SPLIT_TRANSPORT_MIRROR
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_MODS_ENABLE
+#define USB_POLLING_INTERVAL_MS 1
+
+// Encoder: override resolution (try 4 if 2 doesn't register turns)
+#define ENCODER_RESOLUTION 4
