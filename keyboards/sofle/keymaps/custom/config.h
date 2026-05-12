64d2b334ab79efab999a5bf254c25e386d5412d4
Bruno Romero <bruunobromero@gmail.com>
2026-05-12 11:04:08 -0300

    :tada:

--- keyboards/sofle/keymaps/custom/config.h
+++ keyboards/sofle/keymaps/custom/config.h
@@ -8,5 +8,10 @@
 #define SPLIT_MODS_ENABLE
 #define USB_POLLING_INTERVAL_MS 1
 
+// Reduce split sync latency for modifier combos
+#define FORCED_SYNC_THROTTLE_MS 10
+#define QMK_KEYS_PER_SCAN 6
+
 #undef ENCODER_RESOLUTION
-#define ENCODER_RESOLUTION 4
+#define ENCODER_RESOLUTION 2
+#define ENCODER_DIRECTION_FLIP
